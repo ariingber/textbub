3d11d29f9501c735607ba27c1d4226fa66ea495a
Ari Ingber <ariingber@gmail.com>
2016-04-01 16:30:04 -0400

    browers_controller and results view updated.  Now basic logic for searching by name, email, handle, and service name will return all results exactly matching.  Need to implement regexs ect to make results better but working so thats good for now.  New I want to make sure that a reviews can belong to a service provider and can be found and displayed in results

--- textbub_app/app/controllers/browsers_controller.rb
+++ textbub_app/app/controllers/browsers_controller.rb
@@ -10,10 +10,25 @@ class BrowsersController < ApplicationController
   end
 
   def result
-    @display = []
-    @reviews = Review.all
+    @results = []
+    #@reviews = Review.all
     @users = User.all
     @name = params["name"]
+    @email = params["email"]
+    @handle = params["handle"]
+    @service_provided = params["service_provided"]
+    @users.each do |user|
+      if @name == user.name
+        @results.push(user)
+      elsif @email == user.email
+        @results.push(user)
+      elsif @handle == user.handle
+        @results.push(user)
+      elsif @service_provided == user.service_provided
+        @results.push(user)
+      end
+    @results
+    end
   end
 
   def search
