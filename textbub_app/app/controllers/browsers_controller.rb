fc13c9abe6a5259eb07e95912c3d633fe39fe2dc
Ari Ingber <ariingber@gmail.com>
2016-04-04 16:30:41 -0400

    updated browsers_controller, multifield searches are working and searching is avialable for all possible fields

--- textbub_app/app/controllers/browsers_controller.rb
+++ textbub_app/app/controllers/browsers_controller.rb
@@ -18,6 +18,9 @@ class BrowsersController < ApplicationController
     @email = params["email"]
     @handle = params["handle"]
     @service_provided = params["service_provided"]
+    @business_name = params["business_name"]
+    @city = params["city"]
+    @zipcode = params["zipcode"]
     @users.each do |user|
       if @name == user.name
         @results.push(user)
@@ -26,6 +29,12 @@ class BrowsersController < ApplicationController
       elsif @handle == user.handle
         @results.push(user)
       elsif @service_provided == user.service_provided
+        @results.push(user)
+      elsif @business_name == user.business_name
+        @results.push(user)
+      elsif @city == user.city
+        @results.push(user)
+      elsif @zipcode == user.zipcode
         @results.push(user)
       end
     @results
