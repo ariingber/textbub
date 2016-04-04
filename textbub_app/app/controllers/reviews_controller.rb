99463664cea1eb654257f511ce0c0de0fd027b1d
Ari Ingber <ariingber@gmail.com>
2016-04-03 23:18:54 -0400

    changes to reviews_controller to fix multi comment post bug

--- textbub_app/app/controllers/reviews_controller.rb
+++ textbub_app/app/controllers/reviews_controller.rb
@@ -14,18 +14,7 @@ class ReviewsController < ApplicationController
     splitBody = message_body.split()
     handle = splitBody[0]
     from_number = params["From"]
-    @reviews = Review.all
-    if Review.where(handle: handle).empty?
-      @review = Review.create(content: message_body, review_phone: from_number, handle: handle)
-    else
-      Review.where(handle: handle).each do |rev|
-        if rev.review_phone != from_number
-          @review = Review.create(content: message_body, review_phone: from_number, handle: handle)
-        end
-      end
-    end
-
-
+    @review = Review.create(content: message_body, review_phone: from_number, handle: handle)
   end
 
   def show
