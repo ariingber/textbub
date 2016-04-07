2dbba308adc6ae686eb454eeb83517366c28be8b
Ari Ingber <ariingber@gmail.com>
2016-04-07 12:35:08 -0400

    syntax change in review.rb

--- textbub_app/app/models/review.rb
+++ textbub_app/app/models/review.rb
@@ -1,7 +1,7 @@
 class Review < ActiveRecord::Base
   belongs_to :user
 
-  scope :handle_has_number, (handle, number) -> {
+  scope :handle_has_number, -> (handle, number) {
     where(handle: handle, review_phone: number).length > 0
   }
 end
