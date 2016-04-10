4f8b0fdd08f3b9d7161e458bac71daeeffbe921c
Ari Ingber <ariingber@gmail.com>
2016-04-10 15:01:52 -0400

    browsers_controller now ahs better searching so results will show up despite how searcher does capitals

--- textbub_app/app/controllers/browsers_controller.rb
+++ textbub_app/app/controllers/browsers_controller.rb
@@ -14,7 +14,7 @@ class BrowsersController < ApplicationController
     #@reviews = Review.all
     @users = User.all
     @reviews = Review.all
-    @name = params["name"]
+    @name = params["name"].downcase
     @email = params["email"]
     @handle = params["handle"]
     @service_provided = params["service_provided"]
@@ -22,17 +22,17 @@ class BrowsersController < ApplicationController
     @city = params["city"]
     @zipcode = params["zipcode"]
     @users.each do |user|
-      if @name != '' && @name == user.name
+      if @name != '' && @name.downcase == user.name.downcase
         @results.push(user)
-      elsif @email != '' && @email == user.email
+      elsif @email != '' && @email.downcase == user.email.downcase
         @results.push(user)
-      elsif @handle != '' && @handle == user.handle
+      elsif @handle != '' && @handle.downcase == user.handle.downcase
         @results.push(user)
-      elsif @service_provided != '' && @service_provided == user.service_provided
+      elsif @service_provided != '' && @service_provided.downcase == user.service_provided.downcase
         @results.push(user)
-      elsif @business_name != '' && @business_name == user.business_name
+      elsif @business_name != '' && @business_name.downcase == user.business_name.downcase
         @results.push(user)
-      elsif @city != '' && @city == user.city
+      elsif @city != '' && @city.downcase == user.city.downcase
         @results.push(user)
       elsif @zipcode != '' && @zipcode == user.zipcode
         @results.push(user)
